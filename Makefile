f4cec7387fd010de8df17b596ea43a078110cb6e
SanctiBC777 <gorbunovm155@gmail.com>
2013-01-31 12:33:57 +0000

    initial

--- Makefile
+++ Makefile
@@ -1,59 +1,14 @@
-.PHONY: test
 test:
 	python setup.py test
 
-.PHONY: flake8
 flake8:
-	flake8 django_enumfield
+	flake8 --ignore=E501,E225,E128,W391,W404 --max-complexity 12 django_enumfield
 
-.PHONY: mypy
-mypy:
-	mypy django_enumfield
-
-.PHONY: isort
-isort:
-	isort -rc django_enumfield run_tests.py setup.py
-
-.PHONY: black
-black:
-	black django_enumfield run_tests.py setup.py
-
-.PHONY: black-check
-black-check:
-	black --check django_enumfield run_tests.py setup.py
-
-.PHONY: checks
-checks: mypy flake8 black-check
-
-.PHONY: format
-format: black isort
-
-.PHONY: install
 install:
 	python setup.py install
 
-.PHONY: develop
 develop:
 	python setup.py develop
 
-.PHONY: coverage
 coverage:
 	coverage run --include=django_enumfield/* setup.py test
-
-.PHONY: clean
-clean:
-	rm -rf build dist .tox/ *.egg *.egg-info .coverage* .eggs
-	find . -name *.pyc -type f -delete
-	find . -name __pycache__ -type d -delete
-
-.PHONY: build
-build: clean
-	python -m pip install --upgrade pip
-	python -m pip install --upgrade wheel
-	python setup.py sdist bdist_wheel
-
-.PHONY: release
-release: build
-	python -m pip install --upgrade twine
-	python -m twine check dist/*
-	python -m twine upload dist/*
--- django_enumfield/enum.py
+++ django_enumfield/enum.py
@@ -1,228 +1,141 @@
-from __future__ import absolute_import
-
 import logging
-import enum
-from typing import Any, List, Optional, Sequence, Tuple, TypeVar, Union, cast, Mapping
-from django.utils.encoding import force_str
-
-try:
-    from django.utils.functional import classproperty  # type: ignore
-except ImportError:
-    # Pre-Django 3.1
-    from django.utils.decorators import classproperty
-
+from django.utils.encoding import smart_unicode
+from django.utils.translation import gettext as _
 from django_enumfield.db.fields import EnumField
 
-__all__ = ("Enum", "EnumField")
+
+def translate(name):
+    return smart_unicode(_(name.replace('_', ' ').lower())).title()
 
 logger = logging.getLogger(__name__)
-RAISE = object()
 
 
-class BlankEnum(enum.Enum):
-    BLANK = ""
+class EnumType(type):
 
-    @property
-    def label(self):
-        return ""
-
-
-def classdispatcher(class_method):
-    class _classdispatcher(object):
-        def __init__(self, method=None):
-            self.fget = method
-
-        def __get__(self, instance, cls=None):
-            if instance is None:
-                return getattr(cls, class_method)
-            return self.fget(instance)
-
-    return _classdispatcher
-
-
-Default = TypeVar("Default")
-T = TypeVar("T", bound="Enum")
-
-
-class Enum(enum.IntEnum):
-    """A container for holding and restoring enum values"""
-
-    __labels__ = {}  # type: Mapping[int, str]
-    __default__ = None  # type: Optional[int]
-    __transitions__ = {}  # type: Mapping[int, Sequence[int]]
-
-    def __str__(self):
-        return self.label
-
-    @classdispatcher("get_name")
-    def name(self):
-        # type: () -> str
-        return self._name_
-
-    @classdispatcher("get_label")
-    def label(self):
-        # type: () -> str
-        """Get human readable label for the matching Enum.Value.
-        :return: label for value
-        :rtype: str
+    def __new__(cls, *args, **kwargs):
         """
-        labels = self.__class__.__labels__
-        return force_str(labels.get(self.value, self.name))
-
-    @classproperty  # type: ignore[arg-type]
-    def do_not_call_in_templates(cls):
-        # type: () -> bool
-        # Fix for Django templates so that any lookups of enums won't fail
-        # More info: https://stackoverflow.com/questions/35953132/how-to-access-enum-types-in-django-templates  # noqa: E501
-        return True
-
-    @classproperty  # type: ignore[arg-type]
-    def values(cls):
-        # type: () -> Mapping[int, Enum]
-        return {member.value: member for member in cls}  # type: ignore[attr-defined]
-
-    def deconstruct(self):
+        Create enum values from all uppercase class attributes and store them in a dict on the Enum class.
         """
-        See "Adding a deconstruct() method" in
-        https://docs.djangoproject.com/en/1.8/topics/migrations/
+        enum = super(EnumType, cls).__new__(cls, *args, **kwargs)
+        attributes = filter(lambda (k, v): k.isupper(), enum.__dict__.iteritems())
+        enum.values = {}
+        for attribute in attributes:
+            enum.values[attribute[1]] = enum.Value(attribute[0], attribute[1], enum)
+        return enum
+
+
+class Enum(object):
+    """
+    A container for holding and restoring enum values.
+    Usage:
+        class BeerStyle(Enum):
+            LAGER = 0
+            STOUT = 1
+            WEISSBIER = 2
+    It can also validate enum value transitions by defining the _transitions variable as a dict with transitions.
+    """
+    __metaclass__ = EnumType
+
+    class Value(object):
         """
-        c = self.__class__
-        path = "{}.{}".format(c.__module__, c.__name__)
-        return path, [self.value], {}
+        A value represents a key value pair with a uppercase name and a integer value:
+        GENDER = 1
+        "name" is a upper case string representing the class attribute
+        "label" is a translatable human readable version of "name"
+        "enum_type" is the value defined for the class attribute
+        """
+        def __init__(self, name, value, enum_type):
+            self.name = name
+            self.label = translate(name)
+            self.value = value
+            self.enum_type = enum_type
+
+        def __unicode__(self):
+            return unicode(self.label)
+
+        def __str__(self):
+            return self.label
+
+        def __repr__(self):
+            return self.label
+
+        def __eq__(self, other):
+            if other and isinstance(other, Enum.Value):
+                return self.value == other.value
+            else:
+                raise TypeError('Can not compare Enum with %s' % other.__class__.__name__)
 
     @classmethod
-    def items(cls):
-        # type: () -> List[Tuple[str, int]]
+    def choices(cls):
         """
-        :return: List of tuples consisting of every enum value in the form
-            [('NAME', value), ...]
+        Returns a list of tuples with the value as first argument and the value container class as second argument.
         """
-        items = [(member.name, member.value) for member in cls]
-        return sorted(items, key=lambda x: x[1])
-
-    @classmethod
-    def choices(cls, blank=False):
-        # type: (bool) -> List[Tuple[Union[int, str], enum.Enum]]
-        """Choices for Enum
-        :return: List of tuples (<value>, <member>)
-        """
-        choices = sorted(
-            [(member.value, member) for member in cls], key=lambda x: x[0]
-        )  # type: List[Tuple[Union[str, int], enum.Enum]]
-        if blank:
-            choices.insert(0, (BlankEnum.BLANK.value, BlankEnum.BLANK))
-        return choices
+        return sorted(cls.values.iteritems(), key=lambda x: x[0])
 
     @classmethod
     def default(cls):
-        # type: () -> Optional[Enum]
-        """Default Enum value. Set default value to `__default__` attribute
-        of your enum class or override this method if you need another
-        default value.
+        """
+        Returns default value, which is the first one by default.
+        Override this method if you need another default value.
         Usage:
             IntegerField(choices=my_enum.choices(), default=my_enum.default(), ...
-        :return Default value, if set.
         """
-        if cls.__default__ is not None:
-            return cast(Enum, cls(cls.__default__))
-        return None
+        return cls.choices()[0][0]
 
     @classmethod
     def field(cls, **kwargs):
-        # type: (Any) -> EnumField
-        """A shortcut for field declaration
+        """
+        A shortcut for
         Usage:
             class MyModelStatuses(Enum):
                 UNKNOWN = 0
-
             class MyModel(Model):
                 status = MyModelStatuses.field()
-
-        :param kwargs: Arguments passed in EnumField.__init__()
-        :rtype: EnumField
         """
         return EnumField(cls, **kwargs)
 
     @classmethod
-    def get(
-        cls,
-        name_or_numeric,  # type: Union[str, int, T]
-        default=None,  # type: Optional[Default]
-    ):
-        # type: (...) -> Union[Enum, Optional[Default]]
-        """Get Enum.Value object matching the value argument.
-        :param name_or_numeric: Integer value or attribute name
-        :param default: The default to return if the value passed is not
-            a valid enum value
+    def get(cls, name_or_numeric):
         """
-        if isinstance(name_or_numeric, cls):
-            return name_or_numeric
+        Will return a Enum.Value matching the value argument.
+        """
+        if isinstance(name_or_numeric, basestring):
+            name_or_numeric = getattr(cls, name_or_numeric.upper())
 
-        if isinstance(name_or_numeric, int):
-            try:
-                return cls(name_or_numeric)
-            except ValueError:
-                pass
-        elif isinstance(name_or_numeric, str):
-            try:
-                return cls[name_or_numeric]
-            except KeyError:
-                pass
-
-        return default
+        return cls.values.get(name_or_numeric)
 
     @classmethod
-    def get_name(cls, name_or_numeric):
-        # type: (Union[str, int, T]) -> Optional[str]
-        """Get Enum.Value name matching the value argument.
-        :param name_or_numeric: Integer value or attribute name
-        :return: The name or None if not found
+    def name(cls, numeric):
         """
-        value = cls.get(name_or_numeric)
-        if value is not None:
-            return value.name
-        return None
+        Will return the uppercase name for the matching Enum.Value.
+        """
+        return cls.get(numeric).name
 
     @classmethod
-    def get_label(cls, name_or_numeric):
-        # type: (Union[str, int, Enum]) -> Optional[str]
-        """Get Enum.Value label matching the value argument.
-        :param name_or_numeric: Integer value or attribute name
-        :return: The label or None if not found
+    def label(cls, numeric):
         """
-        value = cls.get(name_or_numeric)
-        if value is not None:
-            return value.label
-        return None
+        Will return the human readable label for the matching Enum.Value.
+        """
+        return translate(unicode(cls.get(numeric)))
+
+    @classmethod
+    def items(cls):
+        """
+        Will return a list of tuples consisting of every enum value in the form [('NAME', value), ...]
+        """
+        items = [(value.name, key) for key, value in cls.values.iteritems()]
+        return sorted(items, key=lambda x: x[1])
 
     @classmethod
     def is_valid_transition(cls, from_value, to_value):
-        # type: (Union[int, Enum], Union[int, Enum]) -> bool
-        """Will check if to_value is a valid transition from from_value.
-        Returns true if it is a valid transition.
-
-        :param from_value: Start transition point
-        :param to_value: End transition point
-        :return: Success flag
         """
-        if isinstance(from_value, cls):
-            from_value = from_value.value
-        if isinstance(to_value, cls):
-            to_value = to_value.value
-
-        return (
-            from_value == to_value
-            or not cls.__transitions__
-            or (from_value in cls.transition_origins(to_value))
-        )
+        Will check if to_value is a valid transition from from_value. Returns true if it is a valid transition.
+        """
+        return from_value == to_value or from_value in cls.transition_origins(to_value)
 
     @classmethod
     def transition_origins(cls, to_value):
-        # type: (Union[int, T]) -> Sequence[int]
-        """Returns all values the to_value can make a transition from.
-        :param to_value End transition point
         """
-        if isinstance(to_value, cls):
-            to_value = to_value.value
-
-        return cls.__transitions__.get(to_value, [])
+        Returns all values the to_value can make a transition from.
+        """
+        return cls._transitions[to_value]
