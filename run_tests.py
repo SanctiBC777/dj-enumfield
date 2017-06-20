117705d13c73833db268ebd6d397cbf8cd4a4329
SanctiBC777 <gorbunovm155@gmail.com>
2017-06-20 10:48:31 +0000

    Run tests against Django 1.9+

--- run_tests.py
+++ run_tests.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 
-import os
 import sys
 import django
 from django.conf import settings
 
 
-def delete_migrations():
-    from os.path import exists, abspath, dirname, join
-    migrations_dir = join(
-        dirname(abspath(__file__)), 'django_enumfield', 'tests', 'migrations')
-    if exists(migrations_dir):
-        os.system('rm -r ' + migrations_dir)
-
-
 def main():
-    import warnings
-    warnings.filterwarnings('error', category=DeprecationWarning)
-
-    delete_migrations()
-
     if not settings.configured:
         # Dynamically configure the Django settings with the minimum necessary to
         # get Django running tests
@@ -44,6 +30,7 @@ def main():
             ROOT_URLCONF='django_enumfield.tests.urls',
             DEBUG=True,
             TEMPLATE_DEBUG=True,
+            MIDDLEWARE_CLASSES=[],
         )
 
     # Compatibility with Django 1.7's stricter initialization
@@ -54,11 +41,7 @@ def main():
     test_runner = get_runner(settings)(verbosity=2, interactive=True)
     if '--failfast' in sys.argv:
         test_runner.failfast = True
-
     failures = test_runner.run_tests(['django_enumfield'])
-
-    delete_migrations()
-
     sys.exit(failures)
 
 
