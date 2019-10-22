d89c311f8c75bf18c59248c68b0d4117c72a5151
SanctiBC777 <c7c7c7mura@mail.ru>
2019-10-22 16:42:42 +0000

    Fix initial enum form value
    Previously it the enum was coerced to str and then compared with the int value.
    To solve this we have to create our own form field which coerces the enum value to its value (int), which is then coerced to a str which is fine.

--- django_enumfield/enum.py
+++ django_enumfield/enum.py
@@ -9,8 +9,6 @@ from django.utils.decorators import classproperty
 
 from django_enumfield.db.fields import EnumField
 
-__all__ = ("Enum", "EnumField")
-
 logger = logging.getLogger(__name__)
 RAISE = object()
 
